971262c4690da41915996e34b23c3fa0690e0e7d
Angélica Flores <99211551+Angelicadf97@users.noreply.github.com>
2022-12-21 20:02:22 -0300

    responsividade sobre mim

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
-export default function About (){
-    return(
+export default function About() {
+    return (
         <section>
-        <h1>
-            Sobre mim
-        </h1>
+            <h1>Oi, eu sou a Angélica</h1>
+            <div className="col-lg-6 col-12 mt-4">
+                <p>Sou uma desenvolvedora em início de carreira e também uma jovem arquiteta.</p>
+                <p>Mas aqui vamos focar só na parte que sou uma desenvolvedor, combinado?<br /><small><small>Sobre a jovem arquiteta, hoje virou um hobby.</small></small></p>
+                <p>Amo desafios, então é muito comum me encontrar testando alguma coisa que nunca fiz antes, e junto a isso também estou sempre estimulando minha criatividade, com artesanato, ilustrações e qualquer outra coisa que eu acredite poder fazer<small>(nem sempre sai como o esperado, mas a melhor forma de aprender é errando)</small></p>
+            </div>
         </section>
     )
 }
