21e24cd53e7ce93906397f859f6e84e1453cbc4a
Angélica Flores <99211551+Angelicadf97@users.noreply.github.com>
2022-12-21 20:47:16 -0300

    update fonte

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,8 @@ export default function About() {
             <h1>Oi, eu sou a Angélica</h1>
             <div className="col-lg-6 col-12 mt-4">
                 <p>Sou uma desenvolvedora em início de carreira e também uma jovem arquiteta.</p>
-                <p>Mas aqui vamos focar só na parte que sou uma desenvolvedor, combinado?<br /><small><small>Sobre a jovem arquiteta, hoje virou um hobby.</small></small></p>
-                <p>Amo desafios, então é muito comum me encontrar testando alguma coisa que nunca fiz antes, e junto a isso também estou sempre estimulando minha criatividade, com artesanato, ilustrações e qualquer outra coisa que eu acredite poder fazer<small>(nem sempre sai como o esperado, mas a melhor forma de aprender é errando)</small></p>
+                <p>Mas aqui vamos focar só na parte que sou uma desenvolvedora, combinado?<br /><small><small>A arquitetura virou um hobby.</small></small></p>
+                <p>Amo desafios, então é muito comum me encontrar testando alguma coisa que nunca fiz antes, e junto a isso também estou sempre estimulando minha criatividade, com artesanato, ilustrações e qualquer outra coisa que eu acredite poder fazer<small> (nem sempre sai como o esperado, mas a melhor forma de aprender é errando)</small></p>
             </div>
         </section>
     )
