73b04c95d09cf0e23dcf9f871997fc5540d1986c
Angélica Flores <99211551+Angelicadf97@users.noreply.github.com>
2022-12-22 14:51:08 -0300

    botao dark-mode

--- src/App.js
+++ src/App.js
@@ -20,8 +20,16 @@ function App() {
       <div className="App" id={theme}>
         <Navbar darkmode={
           <div class="form-switch switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={toggleTheme} checked={theme === "dark"} />
-            <label class="form-check-label" for="flexSwitchCheckDefault"></label>
+            <input type="checkbox" id="toggle_checkbox"  onChange={toggleTheme} checked={theme === "dark"} />
+            <label for="toggle_checkbox">
+              <div id="star">
+                <div class="star" id="star-1">★</div>
+                <div class="star" id="star-2">★</div>
+              </div>
+              <div id="moon"></div>
+            </label>
+            {/* <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={toggleTheme} checked={theme === "dark"} />
+            <label class="form-check-label" for="flexSwitchCheckDefault"></label> */}
           </div>
         } />
 
