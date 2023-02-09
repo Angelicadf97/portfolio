2a41d9a2c1588c925d7f62221419f14cac9204b2
Angélica Flores <99211551+Angelicadf97@users.noreply.github.com>
2023-02-09 00:55:49 -0300

    stacks

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -5,18 +5,25 @@ export default function Education() {
                 <h1>
                     Educação
                 </h1>
-                <div>
-                    <h2>Engenharia da Computação</h2>
-                    <p>Faculdade Descomplica 2022/01 - 2025/02</p>
-                </div>
-                <div>
-                    <h2>Arquitetura e Urbanismo</h2>
-                    <p>Universidade do Vale do Rio dos Sinos(UNISINOS) 2015/01 - 2021/01</p>
-                </div>
+                <div className="row">
+                    <div className="col-6 text-end">
+                        <h2>Engenharia da Computação</h2>
+                        <p>Faculdade Descomplica 2022/01 - 2025/02</p>
+                    </div>
 
-                <div>
-                    <h2>Programação Full Stack</h2>
-                    <p>RecodePro 2022/01 - 2022/02</p>
+                </div>
+                <div className="row justify-content-end">
+                    <div className="col-6">
+                        <h2>Arquitetura e Urbanismo</h2>
+                        <p>Universidade do Vale do Rio dos Sinos(UNISINOS) 2015/01 - 2021/01</p>
+                    </div>
+
+                </div>
+                <div className="row">
+                    <div className="col-6 text-end">
+                        <h2>Programação Full Stack</h2>
+                        <p>RecodePro 2022/01 - 2022/02</p>
+                    </div>
                 </div>
             </div>
         </section>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,10 +1,48 @@
+import HTML from '../assets/img/html.png';
+import BootStrap from '../assets/img/bootstrap.png';
+import C from '../assets/img/csharp.png';
+import Figma from '../assets/img/figma.png';
+import Css from '../assets/img/css.png';
+import Ai from '../assets/img/illustrator.png';
+import Java from '../assets/img/java.png';
+import Mongo from '../assets/img/mongodb.png';
+import Mysql from '../assets/img/mysql.png';
+import Ps from '../assets/img/photoshop.png';
+import Python from '../assets/img/python.png';
+import React from '../assets/img/react.png';
+import Spring from '../assets/img/spring.png';
+import Git from '../assets/img/git.png';
+import Js from '../assets/img/js.png';
+
+
 export default function Skills() {
     return (
-        <section id="Skills">
+        <section id="Stacks">
             <div className="container">
                 <h1>
-                    Habilidades
+                    Stacks
                 </h1>
+                <div className='container text-center' >
+                    <img src={HTML} alt="" />
+                    <img src={Css} alt="" />
+                    <img src={Js} alt="" />
+                    <img src={BootStrap} alt="" />
+                    <img src={React} alt="" />
+                </div>
+                <div className='container text-center'>
+                    <img src={Java} alt="" />
+                    <img src={Python} alt="" />
+                    <img src={Spring} alt="" />
+                    <img src={C} alt="" />
+                    <img src={Git} alt="" />
+                </div>
+                <div className='container text-center'>
+                    <img src={Mysql} alt="" />
+                    <img src={Mongo} alt="" />
+                    <img src={Figma} alt="" />
+                    <img src={Ai} alt="" />
+                    <img src={Ps} alt="" />
+                </div>
             </div>
         </section>
     )
